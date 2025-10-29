2c3250bbf26ffdbd74e974a186a6e87ab830d155
Adoramis <luzihan1104@163.com>
2025-10-29 14:39:59 +0800

    2025-10-29

--- LexicographicMinMax/src/Exercises.java
+++ LexicographicMinMax/src/Exercises.java
@@ -7,10 +7,22 @@ public class Exercises {
     */
     public static void printSmallest(String str) {
         // todo
-        
+        String min = "Z";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i, i + 1).compareTo(min) < 0) {
+                min = str.substring(i, i + 1);
+            }
+            if (min.equals("A")) {
+                break;
+            }
+        }
+        System.out.println(min);
     }
 
     public static void main(String[] args) {
         // todo - test
+        printSmallest("PROBLEMS");
+        printSmallest("HELLO");
+        printSmallest("ABSTRACT");
     }
 }
